aed9b3ded58c7d84b6af0d73b57fb8d8683a03c1
xiayh17 <xiayh17@gmail.com>
2022-06-18 19:29:24 +0800

    add pkgdown

--- dev/dev.R
+++ dev/dev.R
@@ -26,6 +26,8 @@ devtools::build_readme()
 
 ## more document
 # usethis::use_pkgdown()
+# usethis::use_pkgdown_github_pages()
+# use_github_action("pkgdown")
 
 ## push to github
 # usethis::use_github()
